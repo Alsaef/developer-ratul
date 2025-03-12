8d8401ffba7a79a4786c3dc666769b4ec48c424d
Alsaef <saef.ratul@gmail.com>
2025-03-12 17:21:08 +0600

    resume url update

--- src/app/resume/page.jsx
+++ src/app/resume/page.jsx
@@ -10,7 +10,7 @@ const page = () => {
                 <h2 className='text-4xl text-center text-white'>My <strong className='text-[var(--color-primary)]'>Resume</strong></h2>
             </div>
             <div className="flex items-center justify-center mt-10">
-                <a href='https://drive.google.com/file/d/1oM7Bw_0TNFLW-BLP4a6KMdcovi6VZYPQ/view?usp=sharing' target='_blank'><button className="px-4 py-2 text-white bg-[var(--color-primary)] rounded flex items-center">
+                <a href='https://drive.google.com/file/d/1gVNhH3vi6oZ2iRe_S5NOn4K-mahqMOKh/view?usp=sharing' target='_blank'><button className="px-4 py-2 text-white bg-[var(--color-primary)] rounded flex items-center">
                 <AiOutlineDownload />  &nbsp; Download Resume
                 </button></a>
             </div>
